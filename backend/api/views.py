2598380c85cd806583b86562b46ffd9910c07a3d
AyeBraine <tushania@gmail.com>
2022-11-07 17:57:55 +0500

    WIP LOCAL fixes

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,19 +9,31 @@ from api.serializers import (IngredientPageSerializer, RecipeReadSerializer,
                              RecipeWriteSerializer, TagSerializer)
 from api.utils import flag_add_delete
 from recipes.models import Cart, Favorite, Ingredient, Recipe, Tag
+from users.models import User
 
 
 class RecipeFilter(dfilters.FilterSet):
     """ Фильтр для вывода рецептов по query parameters. """
-    author = dfilters.CharFilter(field_name='author__id')
-    tags = dfilters.CharFilter(
-        field_name='tags__slug', lookup_expr='icontains')
+    tags = dfilters.ModelMultipleChoiceFilter(
+        field_name='tags__slug', to_field_name='slug',
+        queryset=Tag.objects.all(),)
+    author = dfilters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = dfilters.BooleanFilter(field_name='faved_by')
     is_in_shopping_cart = dfilters.BooleanFilter(field_name='in_cart_for')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags',)
+
+
+class IngredientFilter(dfilters.FilterSet):
+    """ Фильтр для вывода ингредиентов по query parameters. """
+    name = dfilters.CharFilter(field_name='name',
+                               lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'measurement_unit')
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -54,12 +66,12 @@ class IngredientViewSet(viewsets.ModelViewSet):
     """ Вьюсет для вывода страницы со списком ингредиентов. """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientPageSerializer
-    permission_classes = (AuthorAdminOrReadOnly,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('^name',)
+    permission_classes = (permissions.AllowAny,)
+    filterset_class = (IngredientFilter,)
 
 
 class TagViewSet(viewsets.ModelViewSet):
     """ Вьюсет для вывода списка тегов. """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    permission_classes = (permissions.AllowAny,)
--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-sudo docker exec -d foodgram-project-react_web_1 python manage.py migrate
-sudo docker exec -d foodgram-project-react_web_1 python manage.py collectstatic --no-input
-sudo docker exec -d foodgram-project-react_web_1 python manage.py loaddata fixtures.json
-sudo docker exec -d foodgram-project-react_web_1 cp -r static/. backend_static/
+docker exec -d foodgram-project-react-web-1 python manage.py migrate
+docker exec -d foodgram-project-react-web-1 python manage.py collectstatic --no-input
+docker exec -d foodgram-project-react-web-1 python manage.py loaddata fixtures.json
+docker exec -d foodgram-project-react-web-1 cp -r static/. backend_static/
