496ef58440b2de99a83f83ff28216d76c1dff979
AyeBraine <tushania@gmail.com>
2022-11-08 21:32:25 +0500

    AFTERLIFE: Promised Fixes, pt III

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -107,12 +107,12 @@ class RecipeReadSerializer(serializers.ModelSerializer):
 
     def get_queryset(self):
         is_favorited = self.request.query_params.get('is_favorited')
-        if is_favorited is not None and int(is_favorited) == 1:
+        if is_favorited is not None:
             return Recipe.objects.filter(
                 faved_by__user=self.request.user.pk)
         is_in_shopping_cart = self.request.query_params.get(
             'is_in_shopping_cart')
-        if is_in_shopping_cart is not None and int(is_in_shopping_cart) == 1:
+        if is_in_shopping_cart is not None:
             return Recipe.objects.filter(
                 in_cart_for__user=self.request.user.pk)
         return Recipe.objects.all()
